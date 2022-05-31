7ec06a31d5ae550653b121f7e03e57dcad29509a
Hygor Torres Alves <hygortorresalves1999@gmail.com>
2022-05-30 21:43:13 -0300

    Criação das Model iniciais

---
+++ BarberShop/Models/Barbeiros/BarbeiroModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberShop.Models.Barbeiros
+{
+    public class BarbeiroModel
+    {
+        public int Barbeiro_Id { get; set; }
+        public string Name { get; set; }
+        public Services Servicos { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ BarberShop/Models/Barbeiros/Services.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberShop.Models.Barbeiros
+{
+    public class Services
+    {
+        public int Service_Id { get; set; }
+        public bool Cabelo { get; set; }
+        public bool Beard { get; set; }
+        public bool Sombrancelha { get; set; }
+    }
+}
